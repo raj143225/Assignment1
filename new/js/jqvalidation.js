7a0b1848ec87e4e635ab44acab92508c49960dc2
raj143225 <rajkumararisetty1993@gmail.com>
2016-02-01 10:50:32 +0530

    new branch

--- new/js/jqvalidation.js
+++ new/js/jqvalidation.js
@@ -14,16 +14,16 @@ x[7] = numbers_only("#fax","#d11");
 x[8] = numbers_only("#zip1","#15");
 x[9] = numbers_only("#fax1","#d16");
 //restrict values other than numbers
-x[10] = regular_exp('#email','#d3','/^[a-z0-9_-]+@[a-z0-9._-]+\.[a-z]+$/i');
-x[11] = regular_exp("#first_name","#d4","/^[A-Za-z]+$/");
-x[12] = regular_exp("#last_name","#d5","/^[A-Za-z]+$/");
-x[13] = regular_exp("#employer","#d18","/^[A-Za-z]+$/");
-x[14] = regular_exp("#street","#d7","/^[A-Za-z]+$/");
-x[15] = regular_exp("#city","#d8","/^[A-Za-z]+$/");
-x[16] = regular_exp("#state","#d9","/^[A-Za-z]+$/");
-x[17] = regular_exp("#street1","#d12","/^[A-Za-z]+$/");
-x[18] = regular_exp("#city1","#d13","/^[A-Za-z]+$/");
-x[19] = regular_exp("#state","#d14","/^[A-Za-z]+$/");
+x[10] = regular_exp('#email','#d3',/^[a-z0-9_-]+@[a-z0-9._-]+\.[a-z]+$/i);
+x[11] = regular_exp("#first_name","#d4",/^[A-Za-z]+$/);
+x[12] = regular_exp("#last_name","#d5",/^[A-Za-z]+$/);
+x[13] = regular_exp("#employer","#d18",/^[A-Za-z]+$/);
+x[14] = regular_exp("#street","#d7",/^[A-Za-z]+$/);
+x[15] = regular_exp("#city","#d8",/^[A-Za-z]+$/);
+x[16] = regular_exp("#state","#d9",/^[A-Za-z]+$/);
+x[17] = regular_exp("#street1","#d12",/^[A-Za-z]+$/);
+x[18] = regular_exp("#city1","#d13",/^[A-Za-z]+$/);
+x[19] = regular_exp("#state","#d14",/^[A-Za-z]+$/);
 //for regular expression checking
 x[20] = is_empty("#username","#d1");
 x[21] = is_empty("#password","#d2");
